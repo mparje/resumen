c1e8efa7c4977dbd769f72aa7b4fd698f7e964a0
mparje <126131806+mparje@users.noreply.github.com>
2023-04-11 23:28:29 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import openai
+import PyPDF2
+import io
+import requests
+from typing import List
+from dotenv import load_dotenv
+import os
+
+
+# Configura tu clave de API de OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def leer_pdf(archivo_pdf) -> str:
+    lector_pdf = PyPDF2.PdfFileReader(archivo_pdf)
+    texto = ""
+    for num_pagina in range(lector_pdf.numPages):
+        pagina = lector_pdf.getPage(num_pagina)
+        texto += pagina.extractText()
+    return texto
+
+def resumir_texto(texto: str, palabras_maximas: int) -> str:
+    tokens_maximos = palabras_maximas * 1.5  # Estimación para considerar espacios y puntuación
+    respuesta = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Resumen del siguiente texto en aproximadamente {palabras_maximas} palabras:\n\n{texto}\n",
+        max_tokens=int(tokens_maximos),
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    resumen = respuesta.choices[0].text.strip()
+    return resumen
+
+st.title("Resumidor de archivos PDF con IA")
+archivo_subido = st.file_uploader("Sube un archivo PDF para resumir", type=["pdf"])
+palabras_maximas = st.number_input("Introduce la cantidad de palabras en las que deseas que se resuma el texto", min_value=1, value=100)
+
+if archivo_subido:
+    with st.spinner("Resumiendo el archivo PDF..."):
+        archivo_pdf = io.BytesIO(archivo_subido.read())
+        texto = leer_pdf(archivo_pdf)
+        resumen = resumir_texto(texto, palabras_maximas)
+    st.write("Resumen del archivo PDF:")
+    st.write(resumen)
